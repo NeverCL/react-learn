f2e0c57d9f3426c23be59124f59e66e3b39aebd9
Never、C <caolin>
2017-01-02 17:50:40 +0800

    fix Profile

--- 02.jsx/main.jsx
+++ 02.jsx/main.jsx
@@ -1,89 +1,7 @@
-import React, { Component, PropTypes } from 'react';
-import DOM from 'react-dom';
-import { DatePicker, Button, notification } from 'antd';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './Profile';
 import 'antd/dist/antd.css';
-
-class Profile extends Component {
-    constructor(props) {
-        super(props);
-        this.likeFunc = this.likeFunc.bind(this)
-        this.addHobby = this.addHobby.bind(this)
-        this.delHobby = this.delHobby.bind(this)
-        this.state = {
-            liked: 0,
-            hobbies: ['唱歌', '跳舞', '洗碗'],
-            isLoading: false
-        }
-    }
-
-    likeFunc() {
-        let liked = this.state.liked;
-        liked++;
-        this.setState({
-            liked
-        })
-    }
-
-    addHobby() {
-        this.setState({ isLoading: true });
-        let txt = this.refs.hobby.value;
-        if (!txt) {
-            this.setState({ isLoading: false }, () => notification.error({
-                message: '提醒',
-                description: '添加失败,数据为空!',
-            }));
-            return;
-        }
-        let hobbies = this.state.hobbies;
-        hobbies = [...hobbies, txt];
-        this.setState({
-            hobbies
-        });
-        setTimeout(() => this.setState({ isLoading: false }), 500);
-        notification.open({
-            message: '提醒',
-            description: '已添加成功!',
-        });
-        txt = '';
-    }
-
-    delHobby(e) {
-        let hobbies = this.state.hobbies;
-        hobbies.splice(hobbies.indexOf(e.target.innerText), 1);
-        this.setState({
-            hobbies
-        });
-    }
-
-    onChange(date, str) {
-        console.log(date);
-        console.log('当前时间为:' + str);
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>我的名字:{this.props.name}</h1>
-                <h2>我的年龄:{this.props.age}</h2>
-                <Button onClick={this.likeFunc}>给我点赞</Button>
-                <h2>总点赞数:{this.state.liked}</h2>
-                <h2>我的爱好:</h2>
-                <ul>
-                    {this.state.hobbies.map(x => <li key={x} onClick={e => this.delHobby(e)}>{x}</li>)}
-                </ul>
-                <input type="text" ref="hobby" />
-                <Button onClick={this.addHobby} type="primary" icon="plus" loading={this.state.isLoading}>添加爱好</Button>
-                <br />
-                <DatePicker onChange={this.onChange} />
-            </div>
-        )
-    }
-}
-
-Profile.propTypes = {
-    name: PropTypes.string.isRequired,
-    age: PropTypes.number.isRequired
-}
 
 class Container extends Component {
     render() {
@@ -95,5 +13,4 @@ let props = {
     name: 'Mike',
     age: 19
 }
-
-DOM.render(<Container><Profile {...props} /></Container>, document.getElementById('root'));
+ReactDOM.render(<Container><Profile {...props} /></Container>, document.getElementById('root'));
--- 02.jsx/webpack.config.js
+++ 02.jsx/webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");//抽取css文件
 
 module.exports = {
     devServer: {
@@ -16,17 +17,27 @@ module.exports = {
     output: {
         filename: 'bundle.js'
     },
+    resolve: {
+        extensions: ["", ".js", ".jsx", ".tsx"]// 添加需要解析的文件格式
+    },
+    devtool: "source-map",// 启用调试
     module: {
         loaders: [{
             test: /\.jsx$/,
-            loader: 'babel-loader'
+            loader: 'babel'
         }, {
             test: /\.css$/,
-            loader: 'style!css'
-        }]
+            loader: ExtractTextPlugin.extract('style', 'css')
+        },
+            // {
+            //     test: /\.css$/,
+            //     loader: 'style!css'//从右往左执行:css解析,style嵌入到js中
+            // }
+        ]
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),//hot
+        new webpack.HotModuleReplacementPlugin(),//dev hot
+        new ExtractTextPlugin('base.css'),
         new webpack.optimize.CommonsChunkPlugin('vender', 'vender.js')
     ]
 }
